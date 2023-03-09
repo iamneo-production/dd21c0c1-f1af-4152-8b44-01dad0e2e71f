c81f7234fde8e30170def07e52613e01b5311e0e
harikavadali <lakshmiharika23016@gmail.com>
2023-03-09 15:56:53 +0000

    1st commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import './components/uploadJob';
+import uploadJobs from './components/uploadJob';
+
+
+function App() {
+  return (
+    <uploadJobs />
+  );
+}
+
+export default App;
---
+++ client/src/components/uploadJob.jsx
@@ -0,0 +1,12 @@
+
+
+
+
+const uploadJobs = () =>{
+    return(
+        <p> Hello from upload jobs </p>
+    )
+}
+
+
+export default uploadJobs;
